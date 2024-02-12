87a1d56c888ea1e4710651660d85252bc281889d
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2024-02-12 15:32:15 -0300

    Project (Maybe require some adjustments later)

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using Calendar;
+using System.Globalization;
+
+namespace Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateNow = DateTime.Now;
+            List<Task> tasks = new List<Task>();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("([A]dd Task) ([R]emove Task) ([S]ee Tasks) ([C]urrent Time])");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "A":
+                        Console.Clear();
+                        Console.WriteLine("Task Title: ");
+                        string title = Console.ReadLine();
+
+                        Console.Clear();
+                        Console.WriteLine("Task Description: ");
+                        string description = Console.ReadLine();
+
+                        Console.Clear();
+                        Console.WriteLine("Task Date (DD/MM/YYYY))");
+                        string dateStr = Console.ReadLine();
+
+                        Console.Clear();
+                        Console.WriteLine("Task Time (HH:MM))");
+                        string timeStr = Console.ReadLine();
+
+                        string dateTimeCombined = dateStr + " " + timeStr;
+                        if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(description) && !string.IsNullOrEmpty(dateStr) && !string.IsNullOrEmpty(timeStr))
+                        {
+                            if (dateStr.Length < 10 && timeStr.Length < 5)
+                            {
+                                Console.WriteLine("Date or Time is in an invalid format");
+                            }
+                            else
+                            {
+                                string dateHourCombined = dateStr + " " + timeStr;
+                                DateTime dateHour = DateTime.ParseExact(dateHourCombined, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+                                tasks.Add(new Task(title, description, dateHour));
+
+                                Console.WriteLine("Task added sucessfuly");
+                                Console.ReadLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Some fields are missing");
+                            Console.ReadLine();
+                        }
+                        break;
+
+                    case "R":
+                        Console.Clear();
+                        Console.WriteLine("Type the task title that you want to remove: ");
+                        string titleToRemove = Console.ReadLine();
+                        
+                        tasks.RemoveAll(task => task.Title == titleToRemove);
+                        Console.WriteLine("If the title " + titleToRemove + " exists then it will be removed");
+                          
+                        break;
+
+                    case "S":
+                        Console.Clear();
+                        if (tasks.Count > 0)
+                        {
+                            foreach (Task task in tasks)
+                            {
+                                //DateTime dateAndTime = DateTime.ParseExact(dateTimeCombined, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+                                Console.WriteLine(task + "\n");
+                                Console.ReadLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("The list of tasks is empty");
+                            Console.ReadLine();
+                        }
+                        break;
+                    case "C":
+                        Console.WriteLine(dateNow);
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Task
+    {
+        private string _title;
+        private string _description;
+        private DateTime _dateHour;
+
+        public string Title { get => _title; set => _title = value; }
+        public string Description { get => _description; set => _description = value; }
+        public DateTime DateHour { get => _dateHour; set => _dateHour = value; }
+
+        public Task(string title, string description, DateTime dateHour)
+        {
+            _title = title;
+            _description = description;
+            _dateHour = dateHour;
+        }
+
+        public override string ToString()
+        {
+            return "Title: " + Title + "\n" +
+                   "Description: " + Description + "\n" +
+                   "Date and Hour: " + DateHour + "\n";
+                  
+                   
+        }
+
+    }
+}
+
